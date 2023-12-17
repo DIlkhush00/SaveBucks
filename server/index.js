f98e78adb6911fa68d8c94c692019a56ede360bb
Dilkhush Purohit <dilkhushpurohit01@gmail.com>
2023-12-17 23:51:48 +0530

    sets up initial server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+
+// Server listening
+server = app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Export server for testing
+module.exports = server;
