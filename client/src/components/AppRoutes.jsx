d8dd4ba379cd4781ca8448bfb0e8a5654ea7486f
Dilkhush <dilkhushpurohit01@gmail.com>
2025-01-22 17:29:05 +0530

    fix routing

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -24,7 +24,7 @@ const AppRoutes = () => {
 
     return (
         <>
-            <Router>
+            <Router basename='/SaveBucks'>
                 <Routes>
                     <Route exact path="/" element={<HomeBlock />} />
                 </Routes>
