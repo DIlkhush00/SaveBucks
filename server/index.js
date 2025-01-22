6efdc3232be6416e4f6e7ff3b5d9cfadc9622c74
Dilkhush <dilkhushpurohit01@gmail.com>
2025-01-22 19:49:29 +0530

    Update CORS configuration to allow specific origin and methods

--- server/index.js
+++ server/index.js
@@ -20,7 +20,10 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://dilkhush00.github.io"],
+    methods: ["GET", "POST"]
+}));
 
 // Setup socket
 app.set('socketio', io);
